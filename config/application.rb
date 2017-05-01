b9235e54dfdd23fc24ea5fe4707a5d3a22833b11
Rodrigo Rosa <rodrigocrosa@gmail.com>
2017-05-01 19:07:52 -0300

    Home Controller Methods

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-require '.lib/exchange'
+require './lib/exchange'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
